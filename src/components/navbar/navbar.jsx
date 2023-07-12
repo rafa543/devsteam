aed5c7c861cc3d220a7bff4125ce8ec37274b5c0
Rafael Araujo <rafa123839@gmail.com>
2023-07-12 19:01:01 -0300

    criado input

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,3 +1,4 @@
+import Input from "../forms/input/input";
 import Logo from "../logo/logo";
 import styles from "./navbar.module.css";
 import { BsCart4 } from "react-icons/bs";
@@ -6,7 +7,9 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Logo />
-      <input />
+      <div className={styles.search}>
+        <Input type="text" placeholder="Buscar" fullwidth />
+      </div>
       <BsCart4 size={40} />
     </nav>
   );
